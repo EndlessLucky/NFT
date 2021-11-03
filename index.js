284c1f23cd40e131d45f2976eeaf8a55e0c04aef
Olive 105 <105.olivesoft@gmail.com>
2021-11-03 22:38:13 +0200

    feat - first transaction

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const algosdk = require('algosdk');
+const keypress = async () => {
+    process.stdin.setRawMode(true)
+    return new Promise(resolve => process.stdin.once('data', () => {
+        process.stdin.setRawMode(false)
+        resolve()
+    })) 
+}
+// Create an account and add funds to it. Copy the address off
+// The Algorand TestNet Dispenser is located here: 
+// https://dispenser.testnet.aws.algodev.network/
+
 const createAccount =  function (){
     try{  
         const myaccount = algosdk.generateAccount();
@@ -8,9 +19,116 @@ const createAccount =  function (){
         console.log("Account created. Save off Mnemonic and address");
         console.log("Add funds to account using the TestNet Dispenser: ");
         console.log("https://dispenser.testnet.aws.algodev.network/ ");
+
         return myaccount;
     }
     catch (err) {
         console.log("err", err);
     }
 };
+
+/**
+ * Wait until the transaction is confirmed or rejected, or until 'timeout'
+ * number of rounds have passed.
+ * @param {algosdk.Algodv2} algodClient the Algod V2 client
+ * @param {string} txId the transaction ID to wait for
+ * @param {number} timeout maximum number of rounds to wait
+ * @return {Promise<*>} pending transaction information
+ * @throws Throws an error if the transaction is not confirmed or rejected in the next timeout rounds
+ */
+ const waitForConfirmation = async function (algodClient, txId, timeout) {
+    if (algodClient == null || txId == null || timeout < 0) {
+        throw new Error("Bad arguments");
+    }
+
+    const status = (await algodClient.status().do());
+    if (status === undefined) {
+        throw new Error("Unable to get node status");
+    }
+
+    const startround = status["last-round"] + 1;
+    let currentround = startround;
+
+    while (currentround < (startround + timeout)) {
+        const pendingInfo = await algodClient.pendingTransactionInformation(txId).do();
+        if (pendingInfo !== undefined) {
+            if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+                //Got the completed Transaction
+                return pendingInfo;
+            } else {
+                if (pendingInfo["pool-error"] != null && pendingInfo["pool-error"].length > 0) {
+                    // If there was a pool error, then the transaction has been rejected!
+                    throw new Error("Transaction " + txId + " rejected - pool error: " + pendingInfo["pool-error"]);
+                }
+            }
+        }
+        await algodClient.statusAfterBlock(currentround).do();
+        currentround++;
+    }
+    throw new Error("Transaction " + txId + " not confirmed after " + timeout + " rounds!");
+};
+async function firstTransaction() {
+
+    try {
+        let myAccount = createAccount();
+        console.log("Press any key when the account is funded");
+        await keypress();
+        // Connect your client
+        const algodToken = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+        const algodServer = 'http://localhost';
+        const algodPort = 4001;
+        let algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
+
+        //Check your balance
+        let accountInfo = await algodClient.accountInformation(myAccount.addr).do();
+        console.log("Account balance: %d microAlgos", accountInfo.amount);
+        let startingAmount = accountInfo.amount;
+        // Construct the transaction
+        let params = await algodClient.getTransactionParams().do();
+        // comment out the next two lines to use suggested fee
+        params.fee = 1000;
+        params.flatFee = true;
+
+        // receiver defined as TestNet faucet address 
+        const receiver = "HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA";
+        const enc = new TextEncoder();
+        const note = enc.encode("Hello World");
+        let amount = 1000000;
+        let closeout = receiver; //closeRemainderTo
+        let sender = myAccount.addr;
+        let txn = algosdk.makePaymentTxnWithSuggestedParams(sender, receiver, amount, undefined, note, params);
+        // WARNING! all remaining funds in the sender account above will be sent to the closeRemainderTo Account 
+        // In order to keep all remaning funds in the sender account after tx, set closeout parameter to undefined.
+        // For more info see: 
+        // https://developer.algorand.org/docs/reference/transactions/#payment-transaction
+
+        // Sign the transaction
+        let signedTxn = txn.signTxn(myAccount.sk);
+        let txId = txn.txID().toString();
+        console.log("Signed transaction with txID: %s", txId);
+
+        // Submit the transaction
+        await algodClient.sendRawTransaction(signedTxn).do();
+
+        // Wait for confirmation
+        let confirmedTxn = await waitForConfirmation(algodClient, txId, 4);
+        //Get the completed Transaction
+        console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+        // let mytxinfo = JSON.stringify(confirmedTxn.txn.txn, undefined, 2);
+        // console.log("Transaction information: %o", mytxinfo);
+        var string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
+        console.log("Note field: ", string);
+        accountInfo = await algodClient.accountInformation(myAccount.addr).do();
+        console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);        
+        console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
+        let closeoutamt = startingAmount - confirmedTxn.txn.txn.amt - confirmedTxn.txn.txn.fee;        
+        console.log("Close To Amount: %d microAlgos", closeoutamt);
+        console.log("Account balance: %d microAlgos", accountInfo.amount);
+    }
+    catch (err) {
+        console.log("err", err);
+    }
+    process.exit();
+};
+
+firstTransaction();
